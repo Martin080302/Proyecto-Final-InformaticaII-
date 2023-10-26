aeb6061b19896a473f3d0f05e48ec58bc785cd24
Leandro Ponce <leandro.ponce@alumnos.frm.utn.edu.ar>
2023-10-26 20:16:24 -0300

    Se agregan pulsadores para cambiar el mensaje mostrado en el LCD

--- Final Informatica/Lcd/Lcd.ino
+++ Final Informatica/Lcd/Lcd.ino
@@ -1,23 +1,58 @@
-#include <Wire.h>			// libreria de comunicacion por I2C
-#include <LCD.h>			// libreria para funciones de LCD
-#include <LiquidCrystal_I2C.h>		// libreria para LCD por I2C
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd (0x27, 2, 1, 0, 4, 5, 6, 7); // DIR, E, RW, RS, D4, D5, D6, D7
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7);
 
-void setup()
-{
-    lcd.setBacklightPin(3,POSITIVE);	// puerto P3 de PCF8574 como positivo
-    lcd.setBacklight(HIGH);		// habilita iluminacion posterior de LCD
-    lcd.begin(16, 2);			// 16 columnas por 2 lineas para LCD 1602A
-    lcd.clear();			// limpia pantalla
+const int pulsadorIngresoPin = 2;
+const int pulsadorReingresoPin = 3;
+const int pulsadorEgresoPin = 4;
+
+void setup() {
+    lcd.setBacklightPin(3, POSITIVE);
+    lcd.setBacklight(HIGH);
+    lcd.begin(16, 2);
+    lcd.clear();
+   
+
+
+    pinMode(pulsadorIngresoPin, INPUT);
+    pinMode(pulsadorReingresoPin, INPUT);
+    pinMode(pulsadorEgresoPin, INPUT);
 }
 
-void loop()
-{
-    lcd.setCursor(1, 0);		
-    lcd.print("Bienvenido Al Cuartel");				
-    lcd.setCursor(1, 1);		// ubica cursor en columna 2 y linea 0
-    lcd.print("Pulse cualquier boton");			// escribe texto
-    lcd.scrollDisplayLeft();		
-    delay(400);	
-}
+void loop() {
+    
+   
+    lcd.setCursor(2, 0);
+    lcd.print("Bienvenido Al Cuartel  ");
+    lcd.setCursor(2, 1);    
+    lcd.print("Pulse cualquier boton");
+    lcd.scrollDisplayLeft();
+    delay(400);
+
+    if (digitalRead(pulsadorIngresoPin) == HIGH) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Ingreso");
+        delay(2000);
+        lcd.clear();
+    }
+    else if (digitalRead(pulsadorReingresoPin) == HIGH) {
+        lcd.clear();
+        lcd.setCursor(1, 0);
+        lcd.print("Reingreso");
+        delay(2000);
+        lcd.clear();
+    }
+    else if (digitalRead(pulsadorEgresoPin) == HIGH) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Egreso");
+        delay(2000);
+        lcd.clear();
+    }
+
+    
+    
+  }
